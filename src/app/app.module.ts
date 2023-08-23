2097e4b6d05ea6413bffa168981c5d7bebbac964
abdelra0394 <abdlramoh394@gmail.com>
2023-08-23 22:57:44 +0300

    adding the nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContainerComponent
+    ContainerComponent,
+    NavBarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
